7692afc1e8754e791e90f5ef3a7bb11e596153be
JackoDev <35906674+jacko-dev@users.noreply.github.com>
2020-07-11 13:46:38 -0400

    Added variable functionality (of some sort)

--- exec.js
+++ exec.js
@@ -1,25 +1,51 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const { json } = require('express');
 const client = new Discord.Client();
 client.once('ready', () => {
   console.log(`${client.user.tag} has joined the chat.`);
 });
 editorSwitch = 1;
 class Variable {
-	constructor(name, value, type, line) {
-		this.varName = name;
-		this.varValue = value;
+	constructor(type, name, sign, value, end) {
 		this.varType = type;
-		this.varLine = line;
+		this.varName = name;
+		this.varSign = sign;
+		this.varValue = value;
+		this.varEnd = end;
 	}
 }
 client.on('message', async message => {
 	function CheckLine() {
 		editorSwitch = 0;
 		messageCheck = message.content;
-		messageSplit = messageCheck.split(" ");
+		messageSplit = messageCheck.split(/ +/);
 		console.log(messageSplit);
 		editorSwitch = 1;
+		if (messageSplit.includes("var") && messageSplit.indexOf("var") == 0) {
+
+
+			if (messageSplit[3].endsWith(";" || ",")) {
+
+				characterend = messageSplit[3].length - 1;
+				semicolon = messageSplit[3].slice(messageSplit[3].length - 1, messageSplit[3].length);
+				messageSplit[3] = messageSplit[3].slice(0,characterend);
+
+			} else if (messageSplit[3].endsWith("") && !messageSplit[4]) {
+
+				semicolon = "";
+
+			} else if (messageSplit[4] == "stop" || "STOP" || "Stop") {
+
+				semicolon = messageSplit[4];
+
+			}
+
+
+			const variable = new Variable(varType = messageSplit[0], varName = messageSplit[1], varSign = messageSplit[2], varValue = messageSplit[3], varEnd = semicolon);
+			console.log(variable);
+			jsonStringed = JSON.stringify(variable);
+		}
 	};
 	function GoToEditor() {
 		while (editorSwitch == 0) {
